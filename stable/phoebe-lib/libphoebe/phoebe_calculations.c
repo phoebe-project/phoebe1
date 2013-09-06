781b5a70a2090954a44a571c3eb35a02c9b608fb
andrej_prsa <andrej_prsa@a2c7b9f9-86ad-47c3-932c-941601cf3e97>
2013-09-06 20:21:49 +0000

    WD's I/O limitations caused significant figure truncation that affected high precision modeling. This was especially dire for the phase shift and inclination. A framework has been built to work around this issue and it is straight-forward to add any new parameters to this workaround.

--- stable/phoebe-lib/libphoebe/phoebe_calculations.c
+++ stable/phoebe-lib/libphoebe/phoebe_calculations.c
@@ -214,10 +214,14 @@ bool phoebe_pcsv_constrained (int wd_model)
 void intern_call_wd_lc (char *atmcof, char *atmcofplanck, char *lcin, integer *request, integer *nodes, integer *L3perc, double *indep, double *dep, double *ypos, double *zpos)
 {
 	int wd_model;
+    double args[2];
 	double params[14];
 	char *phoebe_model;
 
-	wd_lc (atmcof, atmcofplanck, lcin, request, nodes, L3perc, indep, dep, ypos, zpos, params);
+    phoebe_parameter_get_value (phoebe_parameter_lookup ("phoebe_pshift"), &args[0]);
+    phoebe_parameter_get_value (phoebe_parameter_lookup ("phoebe_incl"),   &args[1]);
+
+	wd_lc (atmcof, atmcofplanck, lcin, request, nodes, L3perc, indep, dep, ypos, zpos, params, args);
 	
 	phoebe_parameter_set_value (phoebe_parameter_lookup ("phoebe_plum1"),   params[ 0]);
 	phoebe_parameter_set_value (phoebe_parameter_lookup ("phoebe_plum2"),   params[ 1]);
