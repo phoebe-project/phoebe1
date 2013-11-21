b03fd2946bab6bd5a03e269fc69a8460479ac594
andrej_prsa <aprsa09@gmail.com>
2013-11-21 13:44:52 +0000

    Binding for light curve computation implemented. Use as:
    	import phoebeBackend as phoebe
    	. . .
    	fluxes = phoebe.lc(tuple_of_indeps, lc_idx)
    	. . .

--- phoebe-py/test.py
+++ phoebe-py/test.py
@@ -19,13 +19,13 @@ phoebe.setlim("phoebe_pot1", 3.0, 5.0)
 
 # Use the 'check' method to see if the parameter is within bounds:
 print "Is pot1 within bounds:", phoebe.check("phoebe_pot1")
-phoebe.setpar("phoebe_pot1", 2.9)
+phoebe.setpar("phoebe_pot1", 2.99)
 print "How about now:        ", phoebe.check("phoebe_pot1")
 
 if not phoebe.check("phoebe_pot1"):
     print "Penalize cost function"
     print "      before:", phoebe.cfval(0)
-    print "       after:", (1-phoebe.check("phoebe_pot1"))*(phoebe.getpar("phoebe_pot1")-pot1)**10*phoebe.cfval(0)
+    print "       after:", (1-phoebe.check("phoebe_pot1"))*(1+(phoebe.getpar("phoebe_pot1")-pot1)**2)**10*phoebe.cfval(0)
 
 # This is a must:
 phoebe.quit()
