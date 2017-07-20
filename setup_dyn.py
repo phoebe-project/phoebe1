24a00a36768953ed3bbdfa37ac4f852c80e0ea80
Martin Horvat <horvat77@gmail.com>
2017-07-20 21:53:14 +0200

    Committing the right version of setup_dyn.py

--- setup_dyn.py
+++ setup_dyn.py
@@ -2,76 +2,36 @@
 
 import sys, glob
 
-if len(sys.argv)>1 and sys.argv[1] == 'build':
-
-  """
-    Making static library
-  """
-
-  #
-  # WD library (libwd)
-  #
-
-  print "WD library (libwd)"
-
-  import numpy.distutils.fcompiler as Fcompiler
-  import numpy.distutils.ccompiler as Ccompiler
-
-  c = Fcompiler.new_fcompiler()
-
-  workdir = './phoebe_legacy/libwd'
-
-  src = glob.glob(workdir+'/*.f')
-
-  # Optionally add include directories etc.
-  c.add_include_dir(workdir)
-
-  c.customize()
-
-  # Compile into .o files
-  objects = c.compile(src, output_dir="./build", extra_preargs=["-fPIC"])
-
-  # Create static library
-  c = Ccompiler.new_compiler()   # linking does not work with fcompiler
-  c.create_static_lib(objects, "wd", output_dir="./build")
-
-
-  #
-  # PHOEBE library (libphoebe)
-  #
-  print "PHOEBE library methods (libphoebe)"
-
-  c = Ccompiler.new_compiler()
-  workdir = "./phoebe_legacy/libphoebe_methods"
-  src = glob.glob(workdir+"/*.c")
-
-  # Optionally add include directories etc.
-  c.add_include_dir(workdir)
-
-  c.set_libraries("wd")
-
-  c.set_library_dirs("./phoebe_legacy/libwd")
-
-  # Compile into .o files
-  objects = c.compile(src, output_dir="./build", extra_preargs=["-fPIC"])
-
-  # Create static
-  c.create_static_lib(objects, "phoebe_methods", output_dir="./build")
-
+build_dir = './build/lib.linux-x86_64-2.7'
+install_dir = '/home/horvat/.local/lib/python2.7/site-packages'
 
 """
   Making python module
 """
 
-from distutils.core import setup, Extension
+from numpy.distutils.core import setup, Extension
 
 ext_modules = [
+    
+    Extension('libwd',
+      sources = glob.glob('./phoebe_legacy/libwd/*.f'),
+      runtime_library_dirs = [install_dir]
+    ),
+    
+    Extension(
+      'libphoebe_methods',
+      sources = glob.glob('./phoebe_legacy/libphoebe_methods/*.c'),
+      library_dirs=[build_dir],
+      libraries = ['wd','gsl','gslcblas','m'],
+    ),
+
     Extension(
       'libphoebe_legacy',
       sources = ['./phoebe_legacy/libphoebe_legacy.c'],
       include_dirs = ['./phoebe_legacy/libphoebe_methods'],
-      extra_link_args= ["-L./build"],
-      libraries = ['phoebe_methods','wd', 'gfortran']
+      libraries = ['phoebe_methods','wd', 'gfortran'],
+      library_dirs=[build_dir],
+      runtime_library_dirs = [install_dir]
     )
   ]
 
