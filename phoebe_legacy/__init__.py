dfe0ef5d64f76b57c265ff492afa3eb9442f2dc2
Martin Horvat <horvat77@gmail.com>
2017-08-09 00:00:59 +0200

    Added phoebeAutoConfigure() == a typical configuration of phoebe_legacy.

--- phoebe_legacy/__init__.py
+++ phoebe_legacy/__init__.py
@@ -1,15 +1,27 @@
 import libphoebe_legacy
 
 import os 
-pathDATA = os.path.dirname(libphoebe_legacy.__file__) + "/phoebe_legacy/tables"
+basedir = os.path.dirname(libphoebe_legacy.__file__) + "/phoebe_legacy"
 
 from libphoebe_legacy import *
-
-# data is the sub-directory tables
-pathWD=pathDATA + "/wd"
-pathPTF=pathDATA + "/ptf"
-pathLD=pathDATA + "/ld"
-pathVH=pathDATA + "/vanhamme"
-
-# load the data
-
+import tempfile
+  
+def phoebeAutoConfigure():
+  
+  startup = os.getcmd()
+  homedir = os.path.expanduser("~")
+  basedir = basedir
+  tmpdir = tempfile.gettempdir()
+  datadir = basedir + "/tables"
+  ptfdir = datadir + "/ptf"
+  ldswitch = 0
+  ldintern = 0
+  lddir =  datadir + "/ld"
+  vhdir =  datadir + "/vanhamme"
+  loadatm = 1
+  dumplco = 1
+  
+  return  phoebeCustomConfigure( bytes(startup),bytes(homedir), 
+            bytes(basedir), bytes(tmpdir), bytes(datadir),
+            bytes(ptfdir), ldswitch, ldintern, bytes(lddir), 
+            bytes(vhdir), loadatm, dumplco)
