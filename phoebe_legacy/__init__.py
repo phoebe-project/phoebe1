41f14b2c70059f28a191faa0eeee2a24412b1d0e
Martin Horvat <horvat77@gmail.com>
2019-03-13 16:46:06 +0100

    Modifying __init__.py in phoebe_legacy.

--- phoebe_legacy/__init__.py
+++ phoebe_legacy/__init__.py
@@ -1,17 +1,24 @@
+import sys
 import libphoebe_legacy
 
-import os 
-packegedir = os.path.dirname(libphoebe_legacy.__file__) + "/phoebe_legacy"
+import os
+packagedir = os.path.dirname(libphoebe_legacy.__file__) + "/phoebe_legacy"
 
 from libphoebe_legacy import *
 import tempfile
-  
+
+
 def auto_configure():
-  
+
+  def f(s):
+    if sys.version_info > (3,):
+      return s
+    else:
+
   startup = os.getcwd()
   homedir = os.path.expanduser("~")
   tmpdir = tempfile.gettempdir()
-  basedir = packegedir + "/tables"
+  basedir = packagedir + "/tables"
   datadir = basedir
   ptfdir = datadir + "/ptf"
   ldswitch = 0
@@ -20,12 +27,22 @@ def auto_configure():
   vhdir =  datadir + "/vanhamme"
   loadatm = 1
   dumplco = 1
-  
+
   verbose_warnings = os.environ.get('VERBOSE_WARNINGS', 1)
   verbose_errors = os.environ.get('VERBOSE_ERRORS', 1)
-  
-  return  custom_configure( bytes(startup),bytes(homedir), 
-            bytes(basedir), bytes(tmpdir), bytes(datadir),
-            bytes(ptfdir), ldswitch, ldintern, bytes(lddir), 
-            bytes(vhdir), loadatm, dumplco, 
-            verbose_warnings, verbose_errors)
+
+
+    return  custom_configure(f(startup), f(homedir),
+              f(basedir), f(tmpdir),f(datadir),
+              f(ptfdir), ldswitch, ldintern, f(lddir),
+              f(vhdir), loadatm, dumplco,
+              verbose_warnings, verbose_errors)
+  else:
+    return  custom_configure(f(startup), f(homedir),
+              f(basedir), f(tmpdir), f(datadir),
+              f(ptfdir), ldswitch, ldintern, f(lddir),
+              f(vhdir), loadatm, dumplco,
+              verbose_warnings, verbose_errors)
+
+
+auto_configure()
