4e7e9c0df8a71c6b2e5e31c30c86b94cec731ed3
bpablo <bert.w.pablo@gmail.com>
2019-04-30 22:38:47 -0400

    change setup file to support mac
    added lines to the setup file to explicitly say where the include directory is.

--- phoebe-py/setup.py
+++ phoebe-py/setup.py
@@ -1,9 +1,11 @@
 from distutils.core import setup, Extension
+import numpy 
 
 module1 = Extension('phoebeBackend',
                     sources = ['phoebe_backend.c'],
-		    libraries = ['phoebe'])
-
+		    libraries = ['phoebe'],
+                    include_dirs=[numpy.get_include()])
+                    
 setup (name = 'PHOEBE backend',
        version = '0.40',
        description = 'PHOEBE python package',
