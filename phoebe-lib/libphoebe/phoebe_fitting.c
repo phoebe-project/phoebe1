f886eadbece1afdae9c6fe006462151a06350a23
Andrej Prsa <aprsa09@gmail.com>
2019-05-16 16:56:22 -0400

    Intrinsic chi2 for RVs fixed. The value was inadvertantly multiplied by 10,000 due to units mismatch. Fixed.

--- phoebe-lib/libphoebe/phoebe_fitting.c
+++ phoebe-lib/libphoebe/phoebe_fitting.c
@@ -954,7 +954,7 @@ int phoebe_minimize_using_dc (FILE *dc_output, PHOEBE_minimizer_feedback *feedba
 	feedback->iters = 1;
 
 	for (i = 0; i < rvno; i++)
-		feedback->chi2s->val[i] = 10000.0*chi2s[i]/params->sigma[i]/params->sigma[i];
+		feedback->chi2s->val[i] = 100.*100.*chi2s[i]/params->sigma[i]/params->sigma[i];
 	for (i = rvno; i < lcno + rvno; i++)
 		feedback->chi2s->val[i] = chi2s[i]/params->sigma[i]/params->sigma[i];
 
