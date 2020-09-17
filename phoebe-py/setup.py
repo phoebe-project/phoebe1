fc4b3d2132a66c62f7d4cc975f5772c739c16f89
Andrej Prsa <aprsa09@gmail.com>
2020-09-16 22:48:23 -0400

    Preparing the build system for the 1.0 release.

--- phoebe-py/setup.py
+++ phoebe-py/setup.py
@@ -1,7 +1,40 @@
 from distutils.core import setup, Extension
+import setuptools
 import numpy 
 
-module1 = Extension('phoebeBackend',
+with open('README.md', 'r') as ld:
+    long_description = ld.read()
+
+setuptools.setup(
+    name='PHOEBE legacy python wrapper',
+    version='1.0',
+    author='Andrej Prsa',
+    author_email='aprsa@villanova.edu',
+    description='PHOEBE legacy python wrapper',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='http://phoebe-project.org',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 3',
+        'Environment :: Console',
+        'Environment :: X11 Applications',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Natural Language :: English',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Mathematics'
+    ],
+
+)
+
+backend = Extension('phoebeBackend',
                     sources = ['phoebe_backend.c'],
 		    libraries = ['phoebe'],
                     include_dirs=[numpy.get_include()])
@@ -9,5 +42,4 @@ module1 = Extension('phoebeBackend',
 setup (name = 'PHOEBE backend',
        version = '0.40',
        description = 'PHOEBE python package',
-       ext_modules = [module1])
-
+       ext_modules = [backend])
