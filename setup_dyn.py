7ddc3cb9bdd3dcc0ef2d9340c515a07c91730b1c
Martin Horvat <horvat77@gmail.com>
2023-07-06 18:27:02 +0200

    Additional simplification of setup.py

--- setup_dyn.py
+++ setup_dyn.py
@@ -1,20 +1,6 @@
 #!/usr/bin/env python
 
 import sys, glob, site
-from sysconfig import get_platform
-
-build_dir = './build/lib.%s-%d.%d' % (get_platform(), sys.version_info[0], sys.version_info[1])
-
-if '--user' in sys.argv:
-  install_dir = site.getusersitepackages()
-else:
-  install_dir = site.getsitepackages()[0]
-
-
-"""
-  Making python module
-"""
-
 from setuptools import Extension, setup
 
 ext_modules = [
@@ -24,9 +10,7 @@ ext_modules = [
       sources = ['./phoebe_legacy/libphoebe_legacy.c','./phoebe_legacy/libwd/wd.c'] +
                 glob.glob('./phoebe_legacy/libphoebe_methods/*.c') ,
       include_dirs = ['./phoebe_legacy/libphoebe_methods'],
-      libraries = ['gsl','gslcblas','m','f2c'],
-      library_dirs=[build_dir],
-      runtime_library_dirs = [install_dir]
+      libraries = ['gsl','gslcblas','m','f2c']
     )
   ]
 
